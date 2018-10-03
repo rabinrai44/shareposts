50f9d01fff27d467e05201b4e5af0bdfb2b36472
Rabin Rai <rabinrai44@gmail.com>
2018-10-03 17:24:32 -0400

    Initial Commit

---
+++ app/config/config.php
@@ -0,0 +1,12 @@
+<?php
+// DB Params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'user1');
+define('DB_PASS', '123456');
+define('DB_NAME', 'shareposts');
+// App Root
+define('APPROOT', dirname(dirname(__FILE__)));
+// URL Root
+define('URLROOT', 'http://localhost/shareposts');
+// Site Name
+define('SITENAME', 'SharePosts');
