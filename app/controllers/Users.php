f1f9c7735aed08ad51d21ee45f69021524a072fc
Rabin Rai <rabinrai44@gmail.com>
2018-10-04 01:50:25 -0400

    Added users class for creating and managing users

---
+++ app/controllers/Users.php
@@ -0,0 +1,35 @@
+<?php
+    class Users extends Controller{
+        public function __construct(){
+
+        }
+
+        /**
+         * Method does handle two things like loading a form
+         * when user go to the register page
+         * another example is register form when you make
+         * a POST from a form request
+         * And it check whether form is submitted or not then process/load the form
+         */    
+        public function register(){
+            // Check for post
+            if($_SERVER['REQUEST_METHOD'] == 'POST'){
+                // Process form
+            } else {
+                // Init data 
+                $data = [
+                    'name' => '',
+                    'email' => '',
+                    'password' => '',
+                    'confirm_password' => '',
+                    'name_err' => '',
+                    'email_err' => '',
+                    'password_err' => '',
+                    'confirm_password_err' => ''
+                ];
+
+                // Load view 
+                $this->view('users/register', $data);
+            }
+        }
+    }
