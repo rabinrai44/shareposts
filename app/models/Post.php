8907a1857c8d2981b6ae1f0fef21009bc375dd19
Rabin Rai <rabinrai44@gmail.com>
2018-10-06 00:21:02 -0400

    Added delete post feature and display welcome after login with username

--- app/models/Post.php
+++ app/models/Post.php
@@ -64,4 +64,17 @@
             return $row;
         }
 
+        public function deletePost($id){
+            $this->db->query('DELETE FROM posts WHERE id = :id');
+            // Bind Value
+            $this->db->bind(':id', $id);
+
+            // Execute
+            if($this->db->execute()){
+                return true;
+            } else{
+                return false;
+            }
+        }
+
     }
